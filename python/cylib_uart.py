402f57599646863eaae9e1742e88c96457cde763
longhuarst <841105197@qq.com>
2019-01-16 23:27:39 +0800

    123

---
+++ python/cylib_uart.py
@@ -0,0 +1,53 @@
+#coding=utf-8
+
+
+code_dectionary =
+
+
+
+
+
+
+
+
+def get_all_uart_list():
+    print("扫描使用到的串口...")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__" :
+
+
+    print("cylib_uart patch")
+
+    get_all_uart_list()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ python/patch.py
@@ -0,0 +1,19 @@
+#coding=utf-8
+
+from python import cylib_uart
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__' :
+
+    print("CYLIB STM32 通用 补丁工具")
+
+
+    cylib_uart.get_all_uart_list()
